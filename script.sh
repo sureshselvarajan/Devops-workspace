fea3ec0c1ba53bf324b18ef25f04ab0d41ef9a28
suresh <sureshparker60@gmail.com>
2024-08-20 15:37:23 +0530

    sh update

---
+++ script.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Server details
+SERVER_USER=ubuntu
+SERVER_HOST=13.232.36.156
+DEPLOY_PATH=/var/www/html/Devops-workspace
+PEM_KEY_PATH=/var/www/html/autodeploy-key.pem
+
+# Git repository details
+REPO_URL=https://github.com/sureshselvarajan/Devops-workspace.git
+BRANCH=release
+
+# Deployment functions
+deploy() {
+  ssh -i "$PEM_KEY_PATH" $SERVER_USER@$SERVER_HOST "
+    cd $DEPLOY_PATH
+    git pull origin $BRANCH
+    # Additional deployment steps, e.g.,
+    # mvn clean package
+    # systemctl restart your_service
+  "
+}
+
+deploy
